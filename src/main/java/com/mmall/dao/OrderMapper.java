261aff46d2b29083ea1d6266b248f81b8e0a513e
林广杰 <378036288@qq.com>
2019-08-27 18:45:42 +0800

    '开发10-6完'

--- src/main/java/com/mmall/dao/OrderMapper.java
+++ src/main/java/com/mmall/dao/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Order;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface OrderMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,6 @@ public interface OrderMapper {
     int updateByPrimaryKeySelective(Order record);
 
     int updateByPrimaryKey(Order record);
+
+    Order selectByUserIdAndOrderNo(@RequestParam(value = "userId") Integer userId, @RequestParam(value = "OrderNo") Long OrderNo);
 }
